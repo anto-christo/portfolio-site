6e3565946e877bddc4e6ea66518ac4b5241b6255
anto-christo <anto.christo.20@gmail.com>
2021-07-11 21:27:34 +0530

    Contact page partial implementation

--- src/Contents/Experiences.js
+++ src/Contents/Experiences.js
@@ -25,6 +25,23 @@ const EXPERIENCES = [
                 </>
             )
         },
+    },
+    {
+        name: "SAP Labs India",
+        logo: require('../Assets/sap.png').default,
+        url: "https://www.sap.com/index.html",
+        position: "Scholar, Associate Software Developer",
+        tenure: "August 2019 - Present",
+        content: () => {
+            return (
+                <>
+                    <p className={JUSTIFY}>At SAP Labs I have got the opportunity to work across multiple projects and domains with various responsibilities.</p>
+                    <p className={JUSTIFY}>Contributed in the development of API automation library for the SAP Enable Now product using <span className={NODEJS.style}>NodeJS</span>.</p>
+                    <p className={JUSTIFY}>Contributed as a Frontend Developer for SAP SuccessFactors and SAP S/4HANA. Have worked on delivering new User Interfaces and also on improvements to existing interfaces to make them more user friendly using <span className={JAVASCRIPT.style}>JavaScript</span>.</p>
+                    <p className={JUSTIFY + " mb-0"}>Currently working as a Full Stack Developer with the SAP Industry Cloud suite of products and have delivered features on both the Frontend and Backend using <span className={JAVASCRIPT.style}>JavaScript</span> and <span className={JAVA.style}>Java</span> respectively.</p>
+                </>
+            )
+        },
     }
 ];
 
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,10 +1,52 @@
+import {
+    Card,
+    Row,
+    Col,
+    Container
+} from 'react-bootstrap';
+import Lottie from 'react-lottie';
 import NavBar from '../Components/NavBar';
+import * as contactAnimation from '../Assets/contact.json';
 
 export default function Contact() {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: contactAnimation.default
+    };
+
     return (
         <>
             <NavBar />
-            <p>Contact</p>
+            <div className="d-flex align-items-center -mt-14">
+                <Container fluid>
+                    <Row>
+                        <Col sm={3}></Col>
+                        <Col sm={6}>
+                            <Card border="light" className="shadow my-3">
+                                <Card.Body className="text-center">
+                                    <Lottie
+                                        options={defaultOptions}
+                                        height={200}
+                                        width={200}
+                                    />
+                                    <p className="text-lg font-semibold mb-0 mt-5">Email</p>
+                                    <p>anto.christo.20@gmail.com</p>
+                                    <p className=" text-lg font-semibold mb-0">Phone / Whatsapp / Telegram</p>
+                                    <p className="mb-0">8652001978</p>
+                                </Card.Body>
+                                <Card.Footer style={{ backgroundColor: "white" }} className="d-flex mx-auto">
+                                    <a className="btn btn-outline-primary btn-sm mx-4" href="www.github.com" target="_blank">GitHub</a>
+                                    <a className="btn btn-outline-primary btn-sm mx-4" href="www.github.com" target="_blank">GitHub</a>
+                                    <a className="btn btn-outline-primary btn-sm mx-4" href="www.github.com" target="_blank">GitHub</a>
+                                    <a className="btn btn-outline-primary btn-sm mx-4" href="www.github.com" target="_blank">GitHub</a>
+                                </Card.Footer>
+                            </Card>                        
+                        </Col>
+                        <Col sm={3}></Col>
+                    </Row>
+                </Container>
+            </div>
         </>
     );
 }
