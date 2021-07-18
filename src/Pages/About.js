87496620bc14a9253b26517f4fe18aa878ee4462
anto-christo <anto.christo.20@gmail.com>
2021-07-18 17:21:38 +0530

    Remove tailwind and modify about page design

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -16,10 +16,26 @@ export default function About() {
         animationData: introAnimation.default
     };
 
+    const mainDivStyle = {
+        marginTop: '-4%'
+    }
+
+    function getFontSize(style) {
+        const SIZE = {
+            mediumHeader: '2.5rem',
+            smallHeader: '2rem',
+            mediumSubHeader: '1.25rem',
+            smallSubHeader: '1rem'
+        }
+        return {
+            fontSize: SIZE[style]
+        }
+    }
+
     return (
         <>
             <NavBar heading={pages[0].text} />
-            <div className="d-flex align-items-center -mt-20">
+            <div className="d-flex align-items-center" style={mainDivStyle}>
                 <Container fluid>
                     <Row>
                         <Col>
@@ -31,13 +47,17 @@ export default function About() {
                         </Col>
                     </Row>
                     <Row>
-                        <Col className="text-center text-3xl md:text-6xl">
-                            <p>Hi, I am Anto</p>
+                        <Col className="text-center">
+                            <p className="d-none d-md-block" style={getFontSize('mediumHeader')}>Hi, I am Anto</p>
+                            <p className="d-block d-md-none" style={getFontSize('smallHeader')}>Hi, I am Anto</p>
                         </Col>
                     </Row>
                     <Row>
-                        <Col className="text-center text-base md:text-2xl mt-3">
-                            <p>
+                        <Col className="text-center mt-1">
+                            <p className="d-none d-md-block" style={getFontSize('mediumSubHeader')}>
+                                I am a software developer specializing in building Web Applications, Games and Mobile Applications.
+                            </p>
+                            <p className="d-block d-md-none" style={getFontSize('smallSubHeader')}>
                                 I am a software developer specializing in building Web Applications, Games and Mobile Applications.
                             </p>
                         </Col>
